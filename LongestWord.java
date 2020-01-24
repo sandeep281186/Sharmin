5dbb7cc96e174d033172b69710bfe584b4eb1a90
sandeep281186 <51852137+sandeep281186@users.noreply.github.com>
2020-01-24 14:36:04 +0530

    documentation

--- LongestWord.java
+++ LongestWord.java
@@ -1,10 +1,16 @@
 public class LongestWord {
 	/**
-	 * @
-	 */	
-	public String getLongestWord(String para) {
+	 * Finds the largest word and returns length/largest word
+	 * @param - 
+	 * 		paragraph 
+	 * Assumptions -
+	 * 		1.There is distinct longest word in the parameter
+	 * 		2.There is not punctuation mark in the parameter
+	 */
+	
+	public String getLongestWord(String paragraph) {
 		
-        String [] words = para.split(" ");
+        String [] words = paragraph.split(" ");
         String longestWord = "";
         for(int i = 0; i < words.length; i++){
         	if(words[i].length() >= longestWord.length()){
